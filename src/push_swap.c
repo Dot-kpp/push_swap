7a841c310df89c79c22049c35e094b025547f832
bigrig <meowleneon.falcon@kppmail.com>
2022-09-20 15:02:20 -0400

    ..

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,6 +1,18 @@
 #include "push_swap.h"
 
 int main (int argc, char **argv) {
-  
 
+    //Take input
+    
+    //validate input - Check for dups, check is ints, check spaces?, check size?
+
+    //sort
+    if (argc > 1)
+    {
+
+    }
+
+    //things to free:
+    //output = ??
+    
 }
